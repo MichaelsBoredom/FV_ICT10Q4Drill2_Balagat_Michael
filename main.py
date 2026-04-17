6a33db938bf4b761c5f0b80ee01ef85fca27463a
Michael Peter <tryfrnz1@gmail.com>
2026-04-17 08:02:04 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from pyscript import display, document
+import numpy as np
+import matplotlib.pyplot as plt
+
+def sample_numpy(e):
+    document.getElementById('output').innerHTML = " "
+
+    calories =  np.array(['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'])
+    burned_calories = np.array([500, 100, 600, 300, 200, 400, 700])
+    graph = plt.plot(calories, burned_calories)
+    plt.show(graph)
+
+    plt.title("Calories Burned by Day")
+    plt.xlabel('Days')
+    plt.ylabel('Calories Burned')
